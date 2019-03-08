fc78053a815ca1f09a2ceea75756bdf18dd5802d
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-03-08 06:51:00 -0600

    lesson-06 commit -- Parsing JSON Manually

--- StarTrivia/StarTrivia/Controller/SelectedPersonVC.swift
+++ StarTrivia/StarTrivia/Controller/SelectedPersonVC.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class SelectedPersonVC: UIViewController {
     
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var massLbl: UILabel!
+    @IBOutlet weak var hairLbl: UILabel!
+    @IBOutlet weak var birthYearLbl: UILabel!
+    @IBOutlet weak var genderLbl: UILabel!
+    
     var personApi = PersonApi()
     
     override func viewDidLoad() {
---
+++ StarTrivia/StarTrivia/Model/Person.swift
@@ -0,0 +1,24 @@
+//
+//  Person.swift
+//  StarTrivia
+//
+//  Created by Nicholas Coggin on 3/8/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    
+    let name: String
+    let height: String
+    let mass: String
+    let hair: String
+    let birthYear: String
+    let gender: String
+    let homeworldUrl: String
+    let filmUrls: [String]
+    let vehicleUrls: [String]
+    let starshipUrls: [String]
+    
+}
