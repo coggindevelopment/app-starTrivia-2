026434631533b033aa720e4f5e164463ff5a1d82
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-03-19 05:53:14 -0500

    lesson-11 commit -- Parsing with SwiftyJSON

--- StarTrivia/StarTrivia/Services/PersonApi.swift
+++ StarTrivia/StarTrivia/Services/PersonApi.swift
@@ -8,10 +8,11 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class PersonApi {
     
-    // Web request with Alamofire
+    // Web request with Alamofire and SwiftJSON
     func getRandomPersonAlamo(id: Int, completion: @escaping PersonResponseCompletion) {
         
         guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
@@ -22,12 +23,35 @@ class PersonApi {
                 return
             }
             
-            guard let json = response.result.value as? [String: Any] else { return
-                completion(nil)}
-            let person = self.parsePersonManual(json: json)
-            completion(person)
+            guard let data = response.data else { return completion(nil)}
+            do {
+                let json = try JSON(data: data)
+                let person = self.parsePersonSwifty(json: json)
+                completion(person)
+            } catch {
+                debugPrint(error.localizedDescription)
+                completion(nil)
             }
         }
+    }
+    
+    // Web request with Alamofire
+//    func getRandomPersonAlamo(id: Int, completion: @escaping PersonResponseCompletion) {
+//
+//        guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
+//        AF.request(url).responseJSON { (response) in
+//            if let error = response.result.error {
+//                debugPrint(error.localizedDescription)
+//                completion(nil)
+//                return
+//            }
+//
+//            guard let json = response.result.value as? [String: Any] else { return
+//                completion(nil)}
+//            let person = self.parsePersonManual(json: json)
+//            completion(person)
+//            }
+//        }
     
     
     // Web request with URL Session
@@ -60,20 +84,37 @@ class PersonApi {
 //        task.resume()
 //    }
 
-    private func parsePersonManual(json: [String: Any]) -> Person {
-        
-        let name = json["name"] as? String ?? ""
-        let height = json["height"] as? String ?? ""
-        let mass = json["mass"] as? String ?? ""
-        let hair = json["hair_color"] as? String ?? ""
-        let birthYear = json["birth_year"] as? String ?? ""
-        let gender = json["gender"] as? String ?? ""
-        let homeworldUrl = json["homeworld"] as? String ?? ""
-        let filmUrls = json["films"] as? [String] ?? [String]()
-        let vehicleUrls = json["vehicles"] as? [String] ?? [String]()
-        let starshipUrls = json["starships"] as? [String] ?? [String]()
+    // Parsing with SwiftyJSON
+    private func parsePersonSwifty(json: JSON) -> Person {
+        let name = json["name"].stringValue
+        let height = json["height"].stringValue
+        let mass = json["mass"].stringValue
+        let hair = json["hair_color"].stringValue
+        let birthYear = json["birth_year"].stringValue
+        let gender = json["gender"].stringValue
+        let homeworldUrl = json["homeworld"].stringValue
+        let filmUrls = json["films"].arrayValue.map({$0.stringValue})
+        let vehicleUrls = json["vehicles"].arrayValue.map({$0.stringValue})
+        let starshipUrls = json["starships"].arrayValue.map({$0.stringValue})
         
         return Person(name: name, height: height, mass: mass, hair: hair, birthYear: birthYear, gender: gender, homeworldUrl: homeworldUrl, filmUrls: filmUrls, vehicleUrls: vehicleUrls, starshipUrls: starshipUrls)
-        
     }
+    
+    // Parsing function using manual methods
+//    private func parsePersonManual(json: JSON) -> Person {
+//
+//        let name = json["name"].stringValue
+//        let height = json["height"].stringValue
+//        let mass = json["mass"].stringValue
+//        let hair = json["hair_color"].stringValue
+//        let birthYear = json["birth_year"].stringValue
+//        let gender = json["gender"].stringValue
+//        let homeworldUrl = json["homeworld"].stringValue
+//        let filmUrls = json["films"].arrayValue.map({$0.stringValue})
+//        let vehicleUrls = json["vehicles"].arrayValue.map({$0.stringValue})
+//        let starshipUrls = json["starships"].arrayValue.map({$0.stringValue})
+//
+//        return Person(name: name, height: height, mass: mass, hair: hair, birthYear: birthYear, gender: gender, homeworldUrl: homeworldUrl, filmUrls: filmUrls, vehicleUrls: vehicleUrls, starshipUrls: starshipUrls)
+//
+//    }
 }
