bd127a9a76c4fb5da5c759f3312a097372f1a142
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-03-22 05:58:58 -0500

    lesson-15 commit -- Setting up additional VCs

---
+++ StarTrivia/StarTrivia/Controller/VehiclesVC.swift
@@ -0,0 +1,29 @@
+//
+//  VehiclesVC.swift
+//  
+//
+//  Created by Nicholas Coggin on 3/22/19.
+//
+
+import UIKit
+
+class VehiclesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
